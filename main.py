08bb37eca929af87a24487bc325f39d15cd5c672
Darek Karasiewicz <dar.karasiewicz@gmail.com>
2023-09-30 15:17:08 +0200

    WIP

---
+++ main.py
@@ -0,0 +1,25 @@
+import random
+
+from easyAI import TwoPlayerGame, Negamax, Human_Player, AI_Player
+
+deck = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+        "Block", "Block", "Block", "Block",
+        "Reverse","Reverse","Reverse","Reverse",
+        "+2", "+2", "+2", "+2",
+        "+4", "+4", "+4", "+4"]
+
+class Uno(TwoPlayerGame):
+    def __init__(self):
+        self.start_hand = []
+        self.start_deck = deck
+        self.deck = deck
+        self.leftover = []
+
+    def get_card(self):
+        random_index= random(len(deck))
+        self.start_hand.append(random_index)
+        #NIE TAK WYWALI ERROR
+        self.start_deck.remove(random_index)
